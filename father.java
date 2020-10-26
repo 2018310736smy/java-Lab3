b98c8a872c0d7e3433f52a2dd29c45058e0168f4
2018310736smy <72729651+2018310736smy@users.noreply.github.com>
2020-10-26 20:42:24 +0800

    Add files via upload

---
+++ father.java
@@ -0,0 +1,16 @@
+package Xuankexitong;
+
+public class father {
+	int   Num;
+	String   Name;
+	String   Sex;
+	int getNum() {
+		return Num;
+		}
+	String getName() {
+		return Name;
+		}
+	String getSex() {
+		return Sex;
+		}
+}
